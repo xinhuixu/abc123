49b276933577df00c96a1a76731e5b4bebcdfb16
Daniel Monteagudo <dmonte3210@gmail.com>
2017-04-21 14:25:39 -0400

    finushed

--- le.py
+++ le.py
@@ -1,15 +1,20 @@
-password = "A$DFE$jjg4583"
+password = "Pa5$word"
 
-def somethingsomething(letter):
+def charMap(letter):
     if letter.isdigit():
         return "num"
     elif letter.isupper():
         return "upper"
     elif letter.islower():
         return "lower"
+    elif letter in ".?!&#,;:-_ *":
+        return "symb"
 
 def simpleCheck(password):
-        makeup = [ somethingsomething(i) for i in password ]
-        return "num" in makeup and "upper" in makeup and "lower" in makeup
+    makeup = [ charMap(i) for i in password ]
+    return "num" in makeup and "upper" in makeup and "lower" in makeup
 
-print simpleCheck(password)
+def passRate(password):
+    makeup = [ charMap(i) for i in password ]
+    lenRating = len(password)/15.0 if len(password) < 15 else 1
+    print (("num" in makeup)*2.5 + ("upper" in makeup)*2.5 + ("lower" in makeup)*2.5 + ("symb" in makeup)*2.5) * lenRating
